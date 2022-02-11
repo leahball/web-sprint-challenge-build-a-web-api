c769d64973f0fee9c4dd4ba44dafc0883dc5bf72
iamleahb <leah.ball@gmail.com>
2022-02-11 17:54:09 -0600

    10 tests passing

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -35,14 +35,29 @@ router.post("/", validateProject, (req, res, next) => {
 //Returns the newly created project as the body of the response.
 //If the request body is missing any of the required fields it responds with a status code 400.
 
-router.put("/:id", validateProjectId, validateProject, (req, res) => {});
+//NOT WORKING
+router.put("/:id", validateProjectId, validateProject, (req, res, next) => {
+  Project.update(req.params.id, {
+    name: req.name,
+    description: req.description,
+  })
+    .then((updatedProject) => {
+      res.json(updatedProject);
+    })
+    .catch(next);
+});
 //`[PUT] /api/projects/:id`
 //Returns the updated project as the body of the response.
 //If there is no project with the given `id` it responds with a status code 404.
 //If the request body is missing any of the required fields it responds with a status code 400.
 
-router.delete("/:id", validateProjectId, (req, res) => {
-  console.log(req.project);
+router.delete("/:id", validateProjectId, async (req, res, next) => {
+  try {
+    await Project.remove(req.params.id);
+    res.json(req.project);
+  } catch (err) {
+    next(err);
+  }
 });
 //`[DELETE] /api/projects/:id`
 //Returns no response body.
@@ -55,4 +70,11 @@ router.get("/:id/actions", validateProjectId, (req, res) => {
 //Returns an array of actions (could be empty) belonging to a project with the given `id`.
 //If there is no project with the given `id` it responds with a status code 404.
 
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    customMessage: "uh oh something bad happened",
+    message: err.message,
+    stack: err.stack,
+  });
+});
 module.exports = router;
