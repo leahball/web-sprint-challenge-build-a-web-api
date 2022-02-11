731b919690730b1c6dfac9f76d7a7b39718e24ff
iamleahb <leah.ball@gmail.com>
2022-02-11 17:36:56 -0600

    post projects working 9 tests passed

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -18,13 +18,19 @@ router.get("/", (req, res, next) => {
 //If there are no projects it responds with an empty array.
 
 router.get("/:id", validateProjectId, (req, res) => {
-  console.log(req.project);
+  res.json(req.project);
 });
 //`[GET] /api/projects/:id`
 //Returns a project with the given `id` as the body of the response.
 //If there is no project with the given `id` it responds with a status code 404.
 
-router.post("/:id", validateProjectId, validateProject, (req, res) => {});
+router.post("/", validateProject, (req, res, next) => {
+  Project.insert({ name: req.name, description: req.description })
+    .then((newProject) => {
+      res.status(201).json(newProject);
+    })
+    .catch(next);
+});
 //`[POST] /api/projects`
 //Returns the newly created project as the body of the response.
 //If the request body is missing any of the required fields it responds with a status code 400.
