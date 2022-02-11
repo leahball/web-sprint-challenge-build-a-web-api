7b2eaad0d7ecef876727c0655dcb8721a959c807
iamleahb <leah.ball@gmail.com>
2022-02-11 15:43:50 -0600

    created scaffolding for actions routers, created scaffolding for middlware for actions and projects

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,4 +1,5 @@
 // add middlewares here related to actions
+const Action = require("./actions-model");
 
 function logger(req, res, next) {
   const timestamp = new Date().toLocaleString();
@@ -8,7 +9,23 @@ function logger(req, res, next) {
   next();
 }
 
-function validateActionsId(req, res, next) {
+async function validateActionsId(req, res, next) {
+  try {
+    const action = await Action.get(req.params.id);
+    if (!action) {
+      res.status(404).json({
+        message: "no such action",
+      });
+    } else {
+      req.action = action;
+      next();
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "problem finding action",
+    });
+  }
+
   console.log("validateActionsId middleware");
   next();
 }
